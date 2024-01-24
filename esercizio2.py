91186f87e7a97dd264404cc1db6807273b7d14ce
@vidalletchonang <vidalletchonang@gmail.com>
2024-01-24 16:38:05 +0100

    esercizi settimana 2 giorno 2

---
+++ esercizio2.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Création d'un tableau NumPy
+arr = np.array([1, 2, 3, 4, 5])
+
+# Affichage du tableau
+print(arr)
+
+# Opération sur le tableau
+arr_squared = arr**2
+print(arr_squared)
